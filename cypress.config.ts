7338c4af7b3f71671d4599b65e0f355cbbe26ee5
Svetlana Stepanova <svetlana.grishanova@gmail.com>
2023-06-08 16:05:25 +0500

    Set initial project settings

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    specPattern: ['src/tests/specs/**/*.ts'],
+    supportFile: false,
+    videosFolder: 'src/videos',
+    screenshotsFolder: 'src/screenshots'
+  },
+  
+});
